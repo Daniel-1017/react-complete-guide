4b885ba4bd644128c38ba65155ea19917fccd5f8
Frimu Daniel <frimudaniel17@gmail.com>
2021-12-13 19:26:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from "react";
-
-const App = () => {
-  return <div></div>;
-};
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
+  );
+}
 
 export default App;
