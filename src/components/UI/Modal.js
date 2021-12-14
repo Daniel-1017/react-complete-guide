42f464c84c1771b5f4ab669615df2de71447633c
Frimu Daniel <frimudaniel17@gmail.com>
2021-12-14 21:29:48 +0100

    141

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -4,7 +4,7 @@ import ReactDom from "react-dom";
 import classes from "./Modal.module.css";
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop}></div>;
+  return <div className={classes.backdrop} onClick={props.onClose}></div>;
 };
 
 const ModalOverlay = (props) => {
@@ -20,7 +20,10 @@ const Modal = (props) => {
 
   return (
     <Fragment>
-      {ReactDom.createPortal(<Backdrop />, portalsElement)}
+      {ReactDom.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalsElement
+      )}
       {ReactDom.createPortal(
         <ModalOverlay> {props.children} </ModalOverlay>,
         portalsElement
