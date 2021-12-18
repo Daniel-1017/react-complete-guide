43010759f03fd022a09c265c80eaaa7feab9f6e7
Frimu Daniel <frimudaniel17@gmail.com>
2021-12-18 11:27:32 +0100

    154

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Button from "./components/UI/Button/Button";
 
 import "./App.css";
+import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
@@ -16,7 +17,7 @@ function App() {
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
+      <DemoOutput show={false} />
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
     </div>
   );
