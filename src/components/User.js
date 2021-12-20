0c8e37e41aa0c542edac70f18beb30406e596ebd
Frimu Daniel <frimudaniel17@gmail.com>
2021-12-20 17:44:09 +0100

    165

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,15 @@
-import classes from './User.module.css';
+import { Component } from "react";
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+import classes from "./User.module.css";
+
+class User extends Component {
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;
