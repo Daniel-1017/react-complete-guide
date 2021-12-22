9d69143804c5dfb919964120a9abf8e92894cd1e
Frimu Daniel <frimudaniel17@gmail.com>
2021-12-22 17:23:09 +0100

    first commit in 15-custom-react-hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React from 'react';
+import BackwardCounter from './components/BackwardCounter';
+import ForwardCounter from './components/ForwardCounter';
 
-const App = () => {
-  return <div></div>;
-};
+function App() {
+  return (
+    <React.Fragment>
+      <ForwardCounter />
+      <BackwardCounter />
+    </React.Fragment>
+  );
+}
 
 export default App;
