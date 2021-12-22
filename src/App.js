251e368cae3b8eb2fcfa6efec8f43f03cef71a38
Frimu Daniel <frimudaniel17@gmail.com>
2021-12-22 20:10:20 +0100

    192 & 193

--- src/App.js
+++ src/App.js
@@ -2,38 +2,34 @@ import React, { useEffect, useState } from "react";
 
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/use-http";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        "https://react-http-c34b1-default-rtdb.firebaseio.com/tasks.json"
+  const transformTasks = (tasksObg) => {
+    const loadedTasks = [];
+
+    for (const taskKey in tasksObg) {
+      loadedTasks.push(
+        { id: taskKey, text: tasksObg[taskKey].text },
+        transformTasks
       );
-
-      if (!response.ok) {
-        throw new Error("Request failed!");
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || "Something went wrong!");
     }
-    setIsLoading(false);
+
+    setTasks(loadedTasks);
   };
+
+  const {
+    isLoading,
+    error,
+    sendRequest: fetchTasks,
+  } = useHttp(
+    {
+      url: "https://react-http-c34b1-default-rtdb.firebaseio.com/tasks.json",
+    },
+    transformTasks
+  );
 
   useEffect(() => {
     fetchTasks();
