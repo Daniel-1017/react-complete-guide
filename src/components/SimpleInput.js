274e9e56e9f12b7ac04e86a3db74ba5e3618f1bb
Frimu Daniel <frimudaniel17@gmail.com>
2021-12-26 19:45:37 +0100

    206

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -7,6 +7,12 @@ const SimpleInput = (props) => {
   const enteredNameIsValid = enteredName.trim() !== "";
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
+  let formIsValid = false;
+
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  }
+
   function nameInputChangeHandler(e) {
     setEnteredName(e.target.value);
   }
@@ -50,7 +56,7 @@ const SimpleInput = (props) => {
         )}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
