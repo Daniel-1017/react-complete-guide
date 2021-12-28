d1928cf9fd93b326856df6e57195ce862fb21011
Frimu Daniel <frimudaniel17@gmail.com>
2021-12-28 15:13:09 +0100

    first commit in 17-http-and-forms-food-order-app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import React from "react";
+import { useState } from 'react';
 
-const App = () => {
-  return <div></div>;
-};
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
+
+function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
+  return (
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+}
 
 export default App;
