a7c769f28d269e0465e11cf307f062e6e48e02fd
Frimu Daniel <frimudaniel17@gmail.com>
2021-12-29 17:19:24 +0100

    236

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,10 @@ import { createStore } from "redux";
 
 const counterReducer = (state = { counter: 0 }, action) => {
   if (action.type === "increment") {
-    return state.counter + 1;
+    return { counter: state.counter + 1 };
   }
   if (action.type === "decrement") {
-    return state.counter - 1;
+    return { counter: state.counter - 1 };
   }
 
   return state;
