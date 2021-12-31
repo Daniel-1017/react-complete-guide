e5336123385cb8f533fa14c267efcee592cd3915
Frimu Daniel <frimudaniel17@gmail.com>
2021-12-31 17:10:45 +0100

    first commit in 19-advanced-redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React from "react";
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
 
-const App = () => {
-  return <div></div>;
-};
+function App() {
+  return (
+    <Layout>
+      <Cart />
+      <Products />
+    </Layout>
+  );
+}
 
 export default App;
