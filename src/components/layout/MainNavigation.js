e9491219ef46bb1f0e380c76f3c2166ec15f30ea
Frimu Daniel <frimudaniel17@gmail.com>
2022-01-03 18:31:54 +0100

    280

---
+++ src/components/layout/MainNavigation.js
@@ -0,0 +1,27 @@
+import React from "react";
+import classes from "./MainNavigation.module.css";
+import { NavLink } from "react-router-dom";
+
+const MainNavigation = () => {
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>Great quotes</div>
+      <nav className={classes.nav}>
+        <ul>
+          <li>
+            <NavLink to="/quotes" activeClassName={classes.active}>
+              All quotes
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/new-quote" activeClassName={classes.active}>
+              Add a quote
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
