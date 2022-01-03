03da54db77ba11bb629ee3f3a9e6fb94d18ef71c
Frimu Daniel <frimudaniel17@gmail.com>
2022-01-03 10:56:39 +0100

    271

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import classes from "./Header.module.css";
 
 const Header = () => {
   return (
-    <header>
+    <header className={classes.header}>
       <nav>
         <ul>
           <li>
-            <Link to="/welcome">Welcome</Link>
+            <NavLink activeClassName={classes.active} to="/welcome">
+              Welcome
+            </NavLink>
           </li>
           <li>
-            <Link to="/products">Products</Link>
+            <NavLink activeClassName={classes.active} to="/products">
+              Products
+            </NavLink>
           </li>
         </ul>
       </nav>
