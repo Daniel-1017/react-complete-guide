382c6d49ece92d864a46cad707ea44f494fd7bf0
Frimu Daniel <frimudaniel17@gmail.com>
2022-01-03 10:47:38 +0100

    270

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { Route } from "react-router-dom";
+import Header from "./components/Header";
 import Products from "./pages/Products";
 import Welcome from "./pages/Welcome";
 
 function App() {
   return (
     <div>
-      <Route path="/welcome">
-        <Welcome />
-      </Route>
-      <Route path="/products">
-        <Products />
-      </Route>
+      <Header />
+      <main>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+      </main>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/welcome">Welcome</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
