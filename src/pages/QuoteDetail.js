66ca59ffd717a6191544929bb91554adec8d8576
Frimu Daniel <frimudaniel17@gmail.com>
2022-01-03 12:48:37 +0100

    278

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { useParams } from "react-router-dom";
 
 const QuoteDetail = () => {
-  return <h1>Quote details page</h1>;
+  const params = useParams();
+
+  console.log(params.quoteId);
+
+  return (
+    <Fragment>
+      <h1>Quote details page</h1>
+      <p> {params.quoteId} </p>
+    </Fragment>
+  );
 };
 
 export default QuoteDetail;
