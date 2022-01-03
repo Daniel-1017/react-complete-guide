ba7f92c821a00d49d8f0c0ae2443ed3a6e239d07
Frimu Daniel <frimudaniel17@gmail.com>
2022-01-03 17:28:17 +0100

    279

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
-import { useParams } from "react-router-dom";
+import Comments from "../components/comments/Comments";
+import { useParams, Route } from "react-router-dom";
 
 const QuoteDetail = () => {
   const params = useParams();
@@ -10,6 +11,9 @@ const QuoteDetail = () => {
     <Fragment>
       <h1>Quote details page</h1>
       <p> {params.quoteId} </p>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
     </Fragment>
   );
 };
