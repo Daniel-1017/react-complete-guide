9ed2922df9e4ec05b45c1920e47268e8cfc6bdfa
Frimu Daniel <frimudaniel17@gmail.com>
2022-01-03 12:11:20 +0100

    275

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Route } from "react-router-dom";
 
 const Welcome = () => {
-  return <h1>Welcome page</h1>;
+  return (
+    <section>
+      <h1>Welcome page</h1>
+      <Route path="/welcome/new-user">
+        <p>Welcome, new user</p>
+      </Route>
+    </section>
+  );
 };
 
 export default Welcome;
