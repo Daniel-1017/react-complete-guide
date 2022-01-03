af992d97ba397c7969ce356d1af03a33508ae312
Frimu Daniel <frimudaniel17@gmail.com>
2022-01-03 12:06:41 +0100

    274

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Products = () => {
   return (
     <section>
       <h1>Products page</h1>
       <ul>
-        <li>A book</li>
-        <li>A carpet</li>
-        <li>An online course</li>
+        <li>
+          <Link to="/products/p1">A book</Link>
+        </li>
+        <li>
+          <Link to="/products/p2">A Carpet</Link>
+        </li>
+        <li>
+          <Link to="/products/p3">A online course</Link>
+        </li>
       </ul>
     </section>
   );
