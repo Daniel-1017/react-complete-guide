f0e88b901ea65a0c76f5cae127c2c7e4251c6f13
Frimu Daniel <frimudaniel17@gmail.com>
2022-01-07 14:34:19 +0100

    first commit in 21-deployng-react-apps

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import AllQuotes from "./pages/AllQuotes";
+import NewQuote from "./pages/NewQuote";
+import NotFound from "./pages/NotFound";
+import QuoteDetail from "./pages/QuoteDetail";
 
-const App = () => {
-  return <div></div>;
-};
+function App() {
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
+        <Route path="/quotes" exact>
+          <AllQuotes />
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <QuoteDetail />
+        </Route>
+        <Route path="/new-quote">
+          <NewQuote />
+        </Route>
+        <Route path="*">
+          <NotFound />
+        </Route>
+      </Switch>
+    </Layout>
+  );
+}
 
 export default App;
---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,69 @@
+import React, { Fragment, useEffect } from "react";
+import Comments from "../components/comments/Comments";
+import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+
+const QuoteDetail = () => {
+  const match = useRouteMatch();
+  const params = useParams();
+
+  const { quoteId } = params;
+
+  /* 
+    here we use object destructuring
+    because that allows us to just pass to quote ID
+    as a dependency instead of the entire parents object,
+    which would mean that whenever any parameter changes
+    this effect would rerun.
+  */
+
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  console.log("match,", match);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered"> {error} </p>;
+  }
+
+  if (!loadedQuote.text) {
+    return <p>No quote found.</p>;
+  }
+
+  return (
+    <Fragment>
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
+      <Route path={match.path} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`${match.url}/comments`}>
+            Load comments
+          </Link>
+        </div>
+      </Route>
+      <Route path={`${match.path}/comments`}>
+        <Comments />
+      </Route>
+    </Fragment>
+  );
+};
+
+export default QuoteDetail;
